f04dd418584371e951221b41cfe065fa2f2cb40b
Daniel Angosto Martínez <daniel.angosto.martinez@gmail.com>
2018-04-26 12:21:05 +0200

    Corrección de error a la hora de eliminar una colección. Ahora se comprueba las cartas de esa colección para modificar su estado publish. (Se dejan en comentarios la explicación de lo que realiza).

--- src/app/components/collections-control-panel/collections-control-panel.component.ts
+++ src/app/components/collections-control-panel/collections-control-panel.component.ts
@@ -128,11 +128,100 @@ export class CollectionsControlPanelComponent implements OnInit {
 
   deleteCollection(id){
     this.collectionDeleted = false;
+    this.collections[id].publish = false;
+    var cardsCheck = this.collections[id].cards.split(',');  //Las cartas de la coleccion que deseo borrar y tengo que comprobar si en el caso de que no haya otra coleccion activa con esa carta la ponga a false
+    var cardsCollection;
+    var check0:boolean = false;
+    var check1:boolean = false;
+    var check2:boolean = false;
+    var check3:boolean = false;
+    var check4:boolean = false;
+    var check5:boolean = false;
+    for(let i=0;i<this.collections.length;i++){  //Recorre todas las coleeciones
+      if(this.collections[i].publish==true){    //Entro si esta en publish true
+        cardsCollection = this.collections[i].cards.split(',');
+        for(let j=0;j<=cardsCollection.length;j++){   //para cada carta de la coleccion publish=true encontrada compruebo si es una de las que estoy buscando, si es asi pongo su check a true para luego saber que esa carta debe permanecer en true y no modificarla
+            if (cardsCollection[j]==cardsCheck[0]){
+                check0=true;
+            }
+            else if (cardsCollection[j]==cardsCheck[1]){
+              check1=true;
+            }
+            else if (cardsCollection[j]==cardsCheck[2]){
+              check2=true;
+            }
+            else if (cardsCollection[j]==cardsCheck[3]){
+              check3=true;
+            }
+            else if (cardsCollection[j]==cardsCheck[4]){
+              check4=true;
+            }
+            else if (cardsCollection[j]==cardsCheck[5]){
+              check5=true;
+            }
+        }
+      }
+    }
+
+    //En el caso de que un check siga a falso es que no hay mas colecciones a true que tengan dicha carta, por lo tanto ponemos dicha carta en publish=false;
+    if (!check0){
+      this._dataService.getItem(cardsCheck[0]).subscribe(data=>{
+          var aux = this.setCard(data['_id'],data['name'],data['history'],data['tags'],data['fileURL'],data['itemType'],false);
+          this._dataService.updateCard(aux);
+      });
+    }
+    if (!check1){
+      this._dataService.getItem(cardsCheck[1]).subscribe(data=>{
+          var aux = this.setCard(data['_id'],data['name'],data['history'],data['tags'],data['fileURL'],data['itemType'],false);
+          this._dataService.updateCard(aux);
+      });
+    }
+    if (!check2){
+      this._dataService.getItem(cardsCheck[2]).subscribe(data=>{
+          var aux = this.setCard(data['_id'],data['name'],data['history'],data['tags'],data['fileURL'],data['itemType'],false);
+          this._dataService.updateCard(aux);
+      });
+    }
+    if (!check3){
+      this._dataService.getItem(cardsCheck[3]).subscribe(data=>{
+          var aux = this.setCard(data['_id'],data['name'],data['history'],data['tags'],data['fileURL'],data['itemType'],false);
+          this._dataService.updateCard(aux);
+      });
+    }
+    if (!check4){
+      this._dataService.getItem(cardsCheck[4]).subscribe(data=>{
+          var aux = this.setCard(data['_id'],data['name'],data['history'],data['tags'],data['fileURL'],data['itemType'],false);
+          this._dataService.updateCard(aux);
+      });
+    }
+    if (!check5){
+      this._dataService.getItem(cardsCheck[5]).subscribe(data=>{
+          var aux = this.setCard(data['_id'],data['name'],data['history'],data['tags'],data['fileURL'],data['itemType'],false);
+          this._dataService.updateCard(aux);
+      });
+    }
+
+
+    //Procedemos a borrar la coleecion
     this._dataService.deleteItem(this.collections[id]._id).subscribe(data=>{
       this.clearData();
       this.collectionDeleted = true;
       this.getAllCollections();
     });
+
+    
+  }
+
+  setCard(_id,name,history,tags,fileURL,itemType, publish) : Card{
+    var cardAux: Card = {_id,name,history,tags,fileURL,itemType, publish};
+    cardAux._id = _id;
+    cardAux.name = name;
+    cardAux.history = history;
+    cardAux.tags = tags;
+    cardAux.fileURL = fileURL;
+    cardAux.itemType = itemType;
+    cardAux.publish = publish;
+    return cardAux;
   }
 
   clearData(){
